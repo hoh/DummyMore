05835f411df7f2dde210c11ccfdba999706d08e5
Hugo Herter <github.dev@hugoh.org>
2014-08-14 01:04:13 +0200

    Added link to download Pidgin source

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 apt-get build-dep pidgin
+#wget http://optimate.dl.sourceforge.net/project/pidgin/Pidgin/2.10.9/pidgin-2.10.9.tar.bz2
 tar -xvjf pidgin-2.10.9.tar.bz2
 ln -s `pwd`/hubbub-pidgin.c pidgin-2.10.9/libpurple/plugins/hubbub-pidgin.c
 cd pidgin-2.10.9
